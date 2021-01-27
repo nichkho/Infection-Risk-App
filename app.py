8c85593a55bf88dcfdc06f967df2ed916338ca2c
Nicholas Kho <43711077+nichkho@users.noreply.github.com>
2021-01-26 19:45:58 -0800

    Update app.py

--- app.py
+++ app.py
@@ -6,16 +6,24 @@ import dash_html_components as html
 
 from calculator import *
 
+rid_path = r'rm.csv'
+room_df = pd.read_csv(rid_path)
+rooms = []
+for rid in room_df['Room']:
+    rooms.append({'label': rid, 'value': rid})
+
 external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
 app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
 
 server = app.server
 activities = [{'label':'Lecture', 'value':'Lecture'}, {'label':'Studying', 'value':'Studying'}, {'label':'Singing', 'value':'Singing'}, {'label':'Social Event', 'value':'Social Event'}, {'label':'Exercising', 'value':'Exercising'}]
+server = app.server
+activities = [{'label':'Lecture', 'value':'Lecture'}, {'label':'Studying', 'value':'Studying'}, {'label':'Singing', 'value':'Singing'}, {'label':'Social', 'value':'Social'}, {'label':'Exercising', 'value':'Exercising'}]
 app.layout = html.Div([
     html.H6("Event Information"),
     #MAKE ROOM ID A DROP DOWN?
     html.Div(["RoomID: ",
-              dcc.Dropdown(id='room-dropdown', value='test', options=[{'label':'hello', 'value':'hello'}])]), #placeholder rooms
+              dcc.Dropdown(id='room-dropdown', value='test', options=rooms)]), #placeholder rooms
     html.Br(),
     html.Div(["Duration of Event (min): ",
               dcc.Input(id='time-input', value = 0, type='number')]),
@@ -38,24 +46,23 @@ app.layout = html.Div([
     dash.dependencies.Output('calc-output', 'children'),
     [dash.dependencies.Input('go-button', 'n_clicks')],
     [dash.dependencies.Input('activity-dropdown', 'value')],
-    [dash.dependencies.Input('room-input', 'value')],
+    [dash.dependencies.Input('room-dropdown', 'value')],
     [dash.dependencies.State('time-input', 'value')],
     [dash.dependencies.State('occupant-input', 'value')]
 )
-def update_calc(n_clicks, activity_dropdown, room_input, time_input, occupant_input, ):
-    # if n_clicks >= 1:
-    #     total_ir = ui_calc(activity_dropdown, room_input, time_input, occupant_input, rid_path)
-    #     total_inf = int(occupant_input * total_ir)
-    #     to_return = 'The risk of holding a(n) {} event for {} minutes in {} is {}%, given the most recent infection rates. With {} occupants, it is likely that {} occupant(s) will be infected.'.format(activity_dropdown, 
-    #                                                                                                                             time_input, 
-    #                                                                                                                             room_input, 
-    #                                                                                                                             round((total_ir * 100),2), 
-    #                                                                                                                             occupant_input,
-    #                                                                                                                             total_inf)
-    #     return to_return
-    # else:
-    return 'Enter Values to get risk calculation'
-
+def update_calc(n_clicks, activity_dropdown, room_input, time_input, occupant_input):
+    if n_clicks >= 1:
+        comp_ir = ui_calc(activity_dropdown, room_input, time_input, occupant_input, rid_path)
+        total_inf = int(occupant_input * comp_ir)
+        to_return = 'The risk of holding a(n) {} event for {} minutes in {} is {}%, given the most recent infection rates. With {} occupants, it is likely that {} occupant(s) will be infected.'.format(activity_dropdown, 
+                                                                                                                                time_input, 
+                                                                                                                                room_input, 
+                                                                                                                                round((comp_ir * 100),2), 
+                                                                                                                                occupant_input,
+                                                                                                                                total_inf)
+        return to_return
+    else:
+        return 'Enter Values to get risk calculation'
 
 if __name__ == '__main__':
     app.run_server(debug=True)
