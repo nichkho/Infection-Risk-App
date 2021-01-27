663d7e96873862d8d0a339418c3ac8e86e6d079c
Nicholas Kho <43711077+nichkho@users.noreply.github.com>
2021-01-26 16:33:07 -0800

    Update app.py

--- app.py
+++ app.py
@@ -35,27 +35,6 @@ app.layout = html.Div([
 
 ])
 
-@app.callback(
-    dash.dependencies.Output('calc-output', 'children'),
-    [dash.dependencies.Input('go-button', 'n_clicks')],
-    [dash.dependencies.Input('activity-dropdown', 'value')],
-    [dash.dependencies.Input('room-input', 'value')],
-    [dash.dependencies.State('time-input', 'value')],
-    [dash.dependencies.State('occupant-input', 'value')]
-)
-def update_calc(n_clicks, activity_dropdown, room_input, time_input, occupant_input, ):
-    # if n_clicks >= 1:
-    #     total_ir = ui_calc(activity_dropdown, room_input, time_input, occupant_input, rid_path)
-    #     total_inf = int(occupant_input * total_ir)
-    #     to_return = 'The risk of holding a(n) {} event for {} minutes in {} is {}%, given the most recent infection rates. With {} occupants, it is likely that {} occupant(s) will be infected.'.format(activity_dropdown, 
-    #                                                                                                                             time_input, 
-    #                                                                                                                             room_input, 
-    #                                                                                                                             round((total_ir * 100),2), 
-    #                                                                                                                             occupant_input,
-    #                                                                                                                             total_inf)
-    #     return to_return
-    # else:
-    return 'Enter Values to get risk calculation'
 
 if __name__ == '__main__':
     app.run_server(debug=True)
