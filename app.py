03a10d24f0946c2b26bd05d40a253d1518ea5f90
Nicholas Kho <43711077+nichkho@users.noreply.github.com>
2021-01-26 16:27:49 -0800

    Update app.py

--- app.py
+++ app.py
@@ -11,11 +11,12 @@ app = JupyterDash(__name__, external_stylesheets=external_stylesheets)
 
 server = app.server
 
+activities = [{'label':'Lecture', 'value':'Lecture'}, {'label':'Studying', 'value':'Studying'}, {'label':'Singing', 'value':'Singing'}, {'label':'Social Event', 'value':'Social Event'}, {'label':'Exercising', 'value':'Exercising'}]
 app.layout = html.Div([
     html.H6("Event Information"),
     #MAKE ROOM ID A DROP DOWN?
     html.Div(["RoomID: ",
-              dcc.Input(id='room-dropdown', value='', options=['hello'])]), #placeholder rooms
+              dcc.Dropdown(id='room-dropdown', value='test', options=[{'label':'hello', 'value':'hello'}])]), #placeholder rooms
     html.Br(),
     html.Div(["Duration of Event (min): ",
               dcc.Input(id='time-input', value = 0, type='number')]),
