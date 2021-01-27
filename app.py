0a7d4db83f0545d5e834597291d332a13cc71d13
Nicholas Kho <43711077+nichkho@users.noreply.github.com>
2021-01-26 17:21:37 -0800

    Update app.py

--- app.py
+++ app.py
@@ -4,8 +4,10 @@ import dash
 import dash_core_components as dcc
 import dash_html_components as html
 
-
-
+sys_path = os.path.dirname(os.getcwd())
+src_path = sys_path + "\\infection_risk_calculator\\src"
+sys.path.insert(1, src_path)
+import calculator as calc
 
 external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
 app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
