f05adad1698069f0426db756d1e807be13f07d80
Nicholas Kho <43711077+nichkho@users.noreply.github.com>
2021-02-08 00:45:15 -0800

    Update app.py

--- app.py
+++ app.py
@@ -4,27 +4,13 @@ import dash
 import dash_core_components as dcc
 import dash_html_components as html
 
-sys_path = os.getcwd()
-src_path = sys_path + "/src"
-
-sys.path.insert(1, src_path)
 from calculator import *
 
-assumptions = var
-
-rid_path = 'rm.csv'
-sys_path = os.getcwd()
-
-
-data_path = sys_path + "/data/"
-
-room_df = pd.read_csv(data_path + rid_path)
-rooms = room_df.copy()
-
-rooms_id = []
+rid_path = r'rm.csv'
+room_df = pd.read_csv(rid_path)
+rooms = []
 for rid in room_df['Room']:
-    
-    rooms_id.append({'label': rid, 'value': rid})
+    rooms.append({'label': rid, 'value': rid})
 
 external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
 app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
@@ -41,12 +27,9 @@ app.layout = html.Div([
     html.Br(),
     html.Div(["Duration of Event (min): ",
               dcc.Input(id='time-input', value = 0, type='number')]),
-    
     html.Br(),
     html.Div(["Number of Occupants: ",
               dcc.Input(id='occupant-input', value = 0, type='number')]),
-    
-    
     html.Br(),
     html.Div(["Activity: ",
               dcc.Dropdown(id='activity-dropdown', value ='test', options=activities)]),
@@ -69,8 +52,8 @@ app.layout = html.Div([
 )
 def update_calc(n_clicks, activity_dropdown, room_input, time_input, occupant_input):
     if n_clicks >= 1:
-        comp_ir = ui_calc(activity_dropdown, room_input, time_input, occupant_input, rid_path, cfm_max = "max")
-        total_inf = int(occupant_input * comp_ir) # Update return according to professor
+        comp_ir = ui_calc(activity_dropdown, room_input, time_input, occupant_input, rid_path)
+        total_inf = int(occupant_input * comp_ir)
         to_return = 'The risk of holding a(n) {} event for {} minutes in {} is {}%, given the most recent infection rates. With {} occupants, it is likely that {} occupant(s) will be infected.'.format(activity_dropdown, 
                                                                                                                                 time_input, 
                                                                                                                                 room_input, 
@@ -81,6 +64,5 @@ def update_calc(n_clicks, activity_dropdown, room_input, time_input, occupant_in
     else:
         return 'Enter Values to get risk calculation'
 
-    
 if __name__ == '__main__':
     app.run_server(debug=True)
