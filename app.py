cec13b682531f9d012c43538d9f30bffb89f4615
Nicholas Kho <43711077+nichkho@users.noreply.github.com>
2021-02-19 13:54:59 -0800

    Update app.py

--- app.py
+++ app.py
@@ -29,8 +29,12 @@ server = app.server
 activities = [{'label':'Lecture', 'value':'Lecture'}, {'label':'Studying', 'value':'Studying'}, {'label':'Singing', 'value':'Singing'}, {'label':'Social', 'value':'Social'}, {'label':'Exercising', 'value':'Exercising'}]
 app.layout = html.Div([
     html.H6("Event Information"),
+    #MAKE ROOM ID A DROP DOWN?
     html.Div(["RoomID: ",
-              dcc.Dropdown(id='room-dropdown', value='test', options=rooms)]), #placeholder rooms
+              dcc.Dropdown(id='room-dropdown', value = list(vav_room.keys())[0],options = [{'label':name, 'value':name} for name in room_names])]),
+    html.Br(),
+    html.Div(["VAV levels: ",
+             dcc.Dropdown(id='vav-dropdown')]),
     html.Br(),
     html.Div(["Duration of Event (min): ",
               dcc.Input(id='time-input', value = 0, type='number')]),
@@ -40,14 +44,7 @@ app.layout = html.Div([
     html.Br(),
     html.Div(["Activity: ",
               dcc.Dropdown(id='activity-dropdown', value ='test', options=activities)]),
-#     html.Br(),
-#     html.Div(["Masks: ",
-#               dcc.RadioItems(id = 'masks-radio', value = 0,
-#     options=[
-#         {'label': 'Masks', 'value': 1},
-#         {'label': 'No Masks', 'value': 0},
-#     ], labelStyle={'display': 'inline-block'}
-# )  ]),
+
     html.Br(),
     html.Button('Go', id = 'go-button', n_clicks = 0),
     html.Br(),
@@ -56,21 +53,26 @@ app.layout = html.Div([
 ])
 
 @app.callback(
+    dash.dependencies.Output('vav-dropdown', 'options'),
+    [dash.dependencies.Input('room-dropdown', 'value')]
+)
+def update_date_dropdown(name):
+    return [{'label': 'cfm min', 'value': vav_room[name][0]}, {'label': 'current', 'value': vav_room[name][2]}, {'label': 'cfm max', 'value': vav_room[name][1]}]
+
+@app.callback(
     dash.dependencies.Output('calc-output', 'children'),
     [dash.dependencies.Input('go-button', 'n_clicks')],
     [dash.dependencies.Input('activity-dropdown', 'value')],
     [dash.dependencies.Input('room-dropdown', 'value')],
-    #[dash.dependencies.Input('masks-radio', 'value')],
+    [dash.dependencies.Input('vav-dropdown', 'value')],
     [dash.dependencies.State('time-input', 'value')],
-    [dash.dependencies.State('occupant-input', 'value')],
-    
+    [dash.dependencies.State('occupant-input', 'value')]
 )
-def update_calc(n_clicks, activity_dropdown, room_input, time_input, occupant_input):
+def update_calc(n_clicks, activity_dropdown, room_input,vav_dropdown, time_input, occupant_input):
     if n_clicks >= 1:
-        comp_ir = ui_calc(activity_dropdown, room_input, time_input, occupant_input, True, rid_path)
-        total_inf = int(occupant_input * comp_ir) 
-        # Update return according to professor
-        to_return = 'The risk of holding a(n) {} event for {} minutes in {} is {}%, given the most recent infection rates. With {} occupants, it is likely that {} occupant(s) will be infected.'.format(activity_dropdown, 
+        comp_ir = ui_calc(activity_dropdown, room_input, time_input, occupant_input,True, rid_path,vav_dropdown)
+        total_inf = int(occupant_input * comp_ir)
+        to_return = 'The risk of an individual infected because of holding a(n) {} event for {} minutes in {} is {}%, given the most recent infection rates. With {} occupants, it is likely that {} occupant(s) will be infected.'.format(activity_dropdown, 
                                                                                                                                 time_input, 
                                                                                                                                 room_input, 
                                                                                                                                 round((comp_ir * 100),2), 
