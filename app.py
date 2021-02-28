702562b7d462c32f64ca2bbf8a8914d5688285ce
Nicholas Kho <43711077+nichkho@users.noreply.github.com>
2021-02-27 19:57:03 -0800

    fix

--- app.py
+++ app.py
@@ -1,4 +1,3 @@
-import plotly.express as px
 import os
 import visdcc
 import sys
@@ -26,15 +25,15 @@ room_names = list(vav_room.keys())
 
 
 external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
-# external_scripts = ['event.js']
+external_scripts = ['event.js']
 
-app = dash.Dash(__name__, external_stylesheets = external_stylesheets)
+app = dash.Dash(__name__, external_stylesheets = external_stylesheets, external_scripts = external_scripts)
 
 server = app.server
 activities = [{'label':'Lecture', 'value':'Lecture'}, {'label':'Studying', 'value':'Studying'}, {'label':'Singing', 'value':'Singing'}, {'label':'Social', 'value':'Social'}, {'label':'Exercising', 'value':'Exercising'}]
 
 
-# data = pd.DataFrame()
+data = pd.DataFrame()
 app.layout = html.Div([
     html.H6("Event Information"),
     html.Div(["RoomID: ",
@@ -58,17 +57,14 @@ app.layout = html.Div([
         {'label': 'Masks', 'value': 1},
         {'label': 'No Masks', 'value': 0},
     ], labelStyle={'display': 'inline-block'})  ]),
-#     html.Br(),
-#     html.Button('Reset', id='reset-button'),
     html.Br(),
     html.Button('Go', id = 'go-button', n_clicks = 0),
     html.Br(),
     html.Div(id = 'calc-output', children = 'Enter values to calculate risk'), 
-#     html.Button("Add to Visualization", id = "addvi", n_clicks = 0), 
-#     visdcc.Run_js(id = 'jct'), 
-#     html.Br(), 
-#     #html.Div([dcc.Graph(id = 'vi')], style={'width': '70%', 'display': 'inline-block', 'padding': '0 20'})
-#     html.Div(id = 'return_value')
+    html.Button("Add to Visualization", id = "addvi", n_clicks = 0), 
+    visdcc.Run_js(id = 'jct'), 
+    html.Br(), 
+#   html.Div(id = 'return_value')
 ])
 
 @app.callback(
@@ -76,21 +72,43 @@ app.layout = html.Div([
     [dash.dependencies.Input('room-dropdown', 'value')]
 )
 def update_date_dropdown(name):
-    return [{'label': 'cfm min', 'value': "min"}, {'label': 'current', 'value': "current"}, {'label': 'cfm max', 'value': "max"}]
-
+        return [{'label': 'Min', 'value': "min"}, {'label': 'Average', 'value': "average"}, 
+            {'label': 'Max', 'value': "max"}, {"label": "Custom", "value": "custom"}]
+    
+@app.callback(
+    dash.dependencies.Output('vav-value', 'value'),
+    [dash.dependencies.Input('vav-dropdown', 'value')] 
+)
+def update_vav(vselect):
+    if vselect == "min": 
+        return "Min"
+    elif vselect == "max": 
+        return "Max"
+    elif vselect == "average": 
+        return "Average"
+    else: 
+        return 0
+    
 @app.callback(
     dash.dependencies.Output('calc-output', 'children'),
     [dash.dependencies.Input('go-button', 'n_clicks')],
     [dash.dependencies.Input('activity-dropdown', 'value')],
     [dash.dependencies.Input('room-dropdown', 'value')],
-    [dash.dependencies.Input('vav-dropdown', 'value')],
+    [dash.dependencies.Input('vav-dropdown', 'value')], 
+    [dash.dependencies.Input('vav-value', 'value')], 
     [dash.dependencies.Input('masks-radio', 'value')],
     [dash.dependencies.State('time-input', 'value')],
     [dash.dependencies.State('occupant-input', 'value')]
 )
-def update_calc(n_clicks, activity_dropdown, room_input,vav_dropdown, mask_tf, time_input, occupant_input):
-    if n_clicks >= 1:
-        comp_ir = ui_calc(activity_dropdown, room_input, time_input, occupant_input, mask_tf, rid_path, vav_dropdown)
+def update_calc(n_clicks, activity_dropdown, room_input,vav_dropdown, vav_value, mask_tf, time_input, occupant_input):
+    if n_clicks >= 1: 
+        if vav_dropdown == "custom": 
+            if (vav_value is None) or (vav_value == ""):
+                return
+            else: 
+                comp_ir = ui_calc(activity_dropdown, room_input, time_input, occupant_input, mask_tf, data_path + rid_path, vav_value)
+        else:           
+            comp_ir = ui_calc(activity_dropdown, room_input, time_input, occupant_input, mask_tf, data_path + rid_path, vav_dropdown)
         total_inf = int(occupant_input * comp_ir)
         to_return = 'The risk of an individual infected because of holding a(n) {} event for {} minutes in {} is {}%, given the most recent infection rates. With {} occupants, it is likely that {} occupant(s) will be infected.'.format(activity_dropdown, 
                                                                                                                                 time_input, 
@@ -100,7 +118,41 @@ def update_calc(n_clicks, activity_dropdown, room_input,vav_dropdown, mask_tf, t
                                                                                                                                 total_inf)
         return to_return
     else:
-        return 'Enter Values to get risk calculation' 
+        return 'Enter Values to get risk calculation'
+
+@app.callback(
+    dash.dependencies.Output('return_value', 'children'),
+    [dash.dependencies.Input('go-button', 'n_clicks')], 
+    [dash.dependencies.Input('activity-dropdown', 'value')],
+    [dash.dependencies.Input('room-dropdown', 'value')],
+    [dash.dependencies.Input('vav-dropdown', 'value')], 
+    [dash.dependencies.Input('vav-value', 'value')],  
+    [dash.dependencies.Input('masks-radio', 'value')],
+    [dash.dependencies.State('time-input', 'value')],
+    [dash.dependencies.State('occupant-input', 'value')])
+def reval(n_clicks, activity_dropdown, room_input, vav_dropdown, vav_value, mask_tf, time_input, occupant_input):
+    if n_clicks >= 1: 
+        if vav_dropdown == "custom": 
+            if (vav_value is None) or (vav_value == ""):
+                return
+            else: 
+                ir = ui_calc(activity_dropdown, room_input, time_input, occupant_input, mask_tf, data_path + rid_path, vav_value)
+                vav = get_vav(data_path + rid_path, room_input, vav_value)
+        else: 
+            ir = ui_calc(activity_dropdown, room_input, time_input, occupant_input, mask_tf, data_path + rid_path, vav_dropdown)
+            vav = get_vav(data_path + rid_path, room_input, vav_dropdown)
+        results = str({"act": activity_dropdown, "rm": room_input, "ti": time_input, "occupants": occupant_input, "masks": mask_tf, 
+                      "vav": vav, "ir": round((ir * 100),2)})
+        return results
+    return ""
+
+@app.callback(
+    dash.dependencies.Output('jct', 'run'),
+    [dash.dependencies.Input('addvi', 'n_clicks')])
+def cross_domain(n_clicks): 
+    if n_clicks >= 1: 
+        return "message()"
+    return "console.log(0)"
    
 
 
