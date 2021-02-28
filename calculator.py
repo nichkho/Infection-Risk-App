26d8c9d5861028d98ab09ca4ed81a3b4aee55b95
Nicholas Kho <43711077+nichkho@users.noreply.github.com>
2021-02-27 16:02:15 -0800

    fix

--- calculator.py
+++ calculator.py
@@ -142,12 +142,12 @@ def infection_risk(t, room_id, n_occupants, activity, expiratory_activity, room_
     
     
     
-#     if cfm == "max":
-#         cfm = room_dic["cfm_max"]
-#     elif cfm == "min":
-#         cfm = room_dic["cfm_min"]
-#     else:
-#         cfm = (room_dic["cfm_max"] + room_dic["cfm_min"]) / 2 # replace using the information current
+    if cfm == "max":
+        cfm = room_dic["cfm_max"]
+    elif cfm == "min":
+        cfm = room_dic["cfm_min"]
+    else:
+        cfm = (room_dic["cfm_max"] + room_dic["cfm_min"]) / 2 # replace using the information current
     #Air Changes per Hour
     
     air_change_rate = get_air_changes_per_hour(cfm, room_dic['room_volume'])
