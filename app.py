9b7d461ba2bc872b7c7e577d8c7d88b80d39194e
HinanawiTS <35885069+HinanawiTS@users.noreply.github.com>
2021-03-10 11:13:51 -0800

    Update app.py

--- app.py
+++ app.py
@@ -62,7 +62,9 @@ app.layout = html.Div([
     html.Div(["VAV Level: ", dcc.Dropdown(id='vav-dropdown')]),
     html.Br(), 
     html.Br(), ##
-    html.Div(["VAV Value: ", dcc.Input(id = "vav-value", value = 0, type = "text"), " CFM. A value of -1 indicates the VAV of given option is unavailable, please input custom VAV or use recommended VAV instead. "]), 
+    html.Div(["VAV Value: ", dcc.Input(id = "vav-value", value = 0, type = "text"), " CFM. "]), 
+    
+    html.Div("A value of -1 indicates the VAV of given option is unavailable, please select "custom" and input custom VAV or select "recommended" and use recommended VAV instead. ")
     html.Br(),
     html.Div(["Air Purifier: ", dcc.Input(id = "air", value = 0, type = "number"), " CFM. "]), 
     html.Br(), 
